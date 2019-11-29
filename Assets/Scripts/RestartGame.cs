c7daa202669d9a5db4ab2edba5d63416d38ab492
AmjadMohamed <amjadmohamed55@gmail.com>
2019-11-29 03:09:57 +0200

    Initial commit

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.transform.tag == "Projectile")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
